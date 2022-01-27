b1fb710a3bdea57619fdd57d7e6ecf98ba4fc9c6
hubert.santos <hubertryanofficial@gmail.com>
2022-01-27 06:48:52 -0300

    updated expot way

--- dist/typescript/dist/module/HeaderContainer/styles.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="styled-components-react-native" />
-export const Container: import("styled-components").StyledComponent<typeof import("react-native").View, import("styled-components").DefaultTheme, {}, never>;
-export const DayContainer: import("styled-components").StyledComponent<typeof import("react-native").View, import("styled-components").DefaultTheme, {}, never>;
-export const DayText: import("styled-components").StyledComponent<typeof import("react-native").Text, import("styled-components").DefaultTheme, {}, never>;
--- dist/typescript/src/utils.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: {
-    primary: string;
-    secundary: string;
-    lightPink: string;
-};
-export default _default;
